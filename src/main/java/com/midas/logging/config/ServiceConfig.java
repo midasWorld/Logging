4c4ee2557dff989fb876ef81e3b608c44f71939a
midasworld <midas117@knou.ac.kr>
2022-06-30 16:56:56 +0900

    chore: querydsl 라이브러리 추가 및 설정

---
+++ src/main/java/com/midas/logging/config/ServiceConfig.java
@@ -0,0 +1,21 @@
+package com.midas.logging.config;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+@Configuration
+public class ServiceConfig {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Bean
+	public JPAQueryFactory jpaQueryFactory() {
+		return new JPAQueryFactory(entityManager);
+	}
+}
